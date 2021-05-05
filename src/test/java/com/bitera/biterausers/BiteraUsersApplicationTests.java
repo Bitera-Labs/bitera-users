8cc0b0956a28cd39cc1fc8ca390e21f0655b3649
Brayan Cruces <brayan2259@gmail.com>
2021-05-05 18:53:56 -0500

    Archivos iniciales de REST

---
+++ src/test/java/com/bitera/biterausers/BiteraUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitera.biterausers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteraUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
